6321bd906695cd70d6f3be1d54715dc4d063bd72
jaytrairat <119471302+jaytrairat@users.noreply.github.com>
2023-06-20 13:56:17 +0700

    Update CenterPictures.vb

--- CenterPictures.vb
+++ CenterPictures.vb
@@ -1,11 +1,13 @@
 Sub CenterPictures()
-    Dim shp As Shape
+   Dim shp As shape
+    pageWidth = ActiveDocument.PageSetup.pageWidth
+    pageMarginLeft = ActiveDocument.PageSetup.LeftMargin
+    pageMarginRight = ActiveDocument.PageSetup.RightMargin
+    pageMargin = (pageWidth - pageMarginLeft - pageMarginRight) / 2
     For Each shp In ActiveDocument.Shapes
         If shp.Type = msoPicture Then
             shp.Select
-            Set shpRange = Selection.ShapeRange
-            shpRange.Align msoAlignCenters, True
-
+            shp.Left = pageMargin - (shp.Width / 2)
         End If
     Next shp
 End Sub
