747bb83761ee84b9d450466704c156cf33dbea00
jaytrairat <119471302+jaytrairat@users.noreply.github.com>
2023-08-03 10:20:17 +0700

    Update CenterGroupsAndImages.vb

--- CenterGroupsAndImages.vb
+++ CenterGroupsAndImages.vb
@@ -4,17 +4,30 @@ Sub CenterGroupsAndImages()
     Dim pageMarginLeft As Double
     Dim pageMarginRight As Double
     Dim pageMargin As Double
-    
+
     pageWidth = ActiveDocument.PageSetup.pageWidth
+    pageHeight = ActiveDocument.PageSetup.pageHeight
+    pageMarginTop = ActiveDocument.PageSetup.TopMargin
+    pageMarginBottom = ActiveDocument.PageSetup.BottomMargin
     pageMarginLeft = ActiveDocument.PageSetup.LeftMargin
     pageMarginRight = ActiveDocument.PageSetup.RightMargin
-    pageMargin = (pageWidth - pageMarginLeft - pageMarginRight) / 2
-    
+
+    contentHeight = pageHeight - pageMarginTop - pageMarginBottom
+    contentWidth = pageWidth - pageMarginLeft - pageMarginRight
+    centerOfPage = contentWidth / 2
+
     For Each shp In ActiveDocument.Shapes
-        If shp.Type = msoGroup Or shp.Type = msoPicture Or shp.Type = msoTextBox Then
-            shp.Left = pageMargin - (shp.Width / 2)
+        If shp.Type = msoGroup Or shp.Type = msoPicture Then
+            shp.Left = centerOfPage - (shp.Width / 2)
+        ElseIf shp.Type = msoTextBox Then
+            If InStr(1, shp.TextFrame.TextRange.Text, "...") > 0 Then
+                shp.Left = contentWidth - (shp.Width)
+                shp.Top = contentHeight
+            Else
+                shp.Left = centerOfPage - (shp.Width / 2)
+            End If
         End If
     Next shp
-    
+
     MsgBox "Centered all shapes"
 End Sub
